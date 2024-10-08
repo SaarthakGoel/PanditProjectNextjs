f75f72e5e44eff6aa41f73b11817f61a43bf6dc8
SaarthakGoel <saarthak145@gmail.com>
2024-10-08 15:40:12 +0530

    yes

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
-//export const pujas = [     pulak dont use these image links , for any doubt contact me
+export const pujas = [
   {
     "puja_id": 1,
     "puja_name": "Ganesh Chaturthi Puja",
     "type": "Festivities",
     "price": 5000,
     "description": "Performed to invoke Lord Ganesha's blessings during the festival of Ganesh Chaturthi, believed to remove obstacles.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/4JvQ8_HeBAV_Chandi%20Havan%20copy_11zon.png"
+    "image": "/pujaListImg/1.png"
   },
   {
     "puja_id": 2,
@@ -13,7 +13,7 @@
     "type": "Festivities",
     "price": 4500,
     "description": "Performed during Diwali to seek blessings from Goddess Lakshmi for wealth and prosperity.",
-    "image": "https://d35pcwl9m9kwu3.cloudfront.net/live/E4gIb_Akhand%20ramayan%20path.jpg"
+    "image": "/pujaListImg/2.jpg"
   },
   {
     "puja_id": 3,
@@ -21,7 +21,7 @@
     "type": "Ceremonial",
     "price": 3500,
     "description": "A ritual to express gratitude and seek blessings for well-being , prosperity and success.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/KVRRK_cN0c1_Annaprashan%20copy.png"
+    "image": "/pujaListImg/3.jpg"
   },
   {
     "puja_id": 4,
@@ -29,7 +29,7 @@
     "type": "Ceremonial",
     "price": 6000,
     "description": "Housewarming ceremony performed before moving into a new home for purifying the space.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/tHZsL_Marriage%20Anniversary_11zon.jpg"
+    "image": "/pujaListImg/4.jpg"
   },
   {
     "puja_id": 5,
@@ -37,7 +37,7 @@
     "type": "Dosha Nivaran Havan & Jaap",
     "price": 7500,
     "description": "Recitation of Maha Mrityunjaya Mantra to overcome obstacles, diseases, and untimely death.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/wci1m_SHANj_Ayush%20Havan_11zon%20%281%29%20copy.png"
+    "image": "/pujaListImg/5.jpg"
   },
   {
     "puja_id": 6,
@@ -45,7 +45,7 @@
     "type": "Marriage",
     "price": 25000,
     "description": "A traditional Hindu wedding ritual that solemnizes the union of bride and groom according to Vedic principles.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/FL2lA_Untitled-1%20copy.png"
+    "image": "/pujaListImg/6.jpg"
   },
   {
     "puja_id": 7,
@@ -53,7 +53,7 @@
     "type": "Birth Related",
     "price": 4000,
     "description": "Naming ceremony for a newborn, performed to bless the child with a healthy and prosperous future.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/B0oMd_YL6LZ_Janam%20Din%20Puja_11zon%20copy_11zon_11zon%20%281%29.jpg"
+    "image": "/pujaListImg/7.jpg"
   },
   {
     "puja_id": 8,
@@ -61,7 +61,7 @@
     "type": "Death Rituals (Ancestral Rites)",
     "price": 15000,
     "description": "Funeral rites performed to ensure the peaceful departure of the soul and its journey to the afterlife.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/d9gpZ_Ueazr_t0dkQ_Brihaspati%20Vrat%20Udyapan%20copy_11zon.png"
+    "image": "/pujaListImg/8.jpg"
   },
   {
     "puja_id": 9,
@@ -69,7 +69,7 @@
     "type": "Business",
     "price": 5500,
     "description": "Performed on Dhanteras to worship Goddess Lakshmi for financial prosperity in business.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/Phvfl_Budh_11zon%20copy_11zon.jpg"
+    "image": "/pujaListImg/9.jpg"
   },
   {
     "puja_id": 10,
@@ -77,7 +77,7 @@
     "type": "Dosha Nivaran Havan & Jaap",
     "price": 6500,
     "description": "Performed to pacify the nine planets and reduce their negative influence in one's life.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/eRpRO_gabkz_mJ1xY_Durga%20Shapashati%20copy_11zon.png"
+    "image": "/pujaListImg/10.png"
   },
   {
     "puja_id": 11,
@@ -85,7 +85,7 @@
     "type": "Festivities",
     "price": 4500,
     "description": "Performed to seek the blessings of Goddess Saraswati for knowledge, wisdom, and artistic pursuits.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/XyZZF_Yk0a6_1nzd3_Sagai_11zon-removebg-preview%20copy.png"
+    "image": "/pujaListImg/1.png"
   },
   {
     "puja_id": 12,
@@ -93,7 +93,7 @@
     "type": "Festivities",
     "price": 5000,
     "description": "Performed on Makar Sankranti to celebrate the sun's transition and to bring in a prosperous harvest.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/5OyY3_AmcOc_Mahalaxmi%20Puja%20%281%29_11zon%20copy.png"
+    "image": "/pujaListImg/2.jpg"
   },
   {
     "puja_id": 13,
@@ -101,7 +101,7 @@
     "type": "Dosha Nivaran Havan & Jaap",
     "price": 8000,
     "description": "A special ritual to remove the malefic effects of Kaal Sarp Dosh in one's horoscope.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/xaAre_f1qI6_GPPOy_Ganapati%20Havan%20copy_11zon%20%281%29.png"
+    "image": "/pujaListImg/3.jpg"
   },
   {
     "puja_id": 14,
@@ -109,7 +109,7 @@
     "type": "Ceremonial",
     "price": 9000,
     "description": "The sacred thread ceremony symbolizing the initiation of young boys into the study of Vedas and spirituality.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/wwyHU_tbIel_rc4Eh_IMG-20221020-WA0008%20copy_11zon_11zon.jpg"
+    "image": "/pujaListImg/4.jpg"
   },
   {
     "puja_id": 15,
@@ -117,7 +117,7 @@
     "type": "Festivities",
     "price": 3000,
     "description": "A Vedic festival dedicated to the Sun God, performed to thank the sun for life on earth and to seek blessings.",
-    "image": "https://d35pcwl9m9kwu3.cloudfront.net/live/aw5Hc_Garud.jpg"
+    "image": "/pujaListImg/5.jpg"
   },
   {
     "puja_id": 16,
@@ -125,7 +125,7 @@
     "type": "Birth Related",
     "price": 3500,
     "description": "The ceremony where a baby is fed solid food for the first time, performed to bless the child with health.",
-    "image": "https://d35pcwl9m9kwu3.cloudfront.net/live/PYEjQ_Kaala%20Sarpa%20Dosha%20Nivarana%20Homam.jpg"
+    "image": "/pujaListImg/6.jpg"
   },
   {
     "puja_id": 17,
@@ -133,7 +133,7 @@
     "type": "Ceremonial",
     "price": 5000,
     "description": "A ritual performed to honor the Earth before starting construction on a new home or building.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/eRpRO_gabkz_mJ1xY_Durga%20Shapashati%20copy_11zon.png"
+    "image": "/pujaListImg/7.jpg"
   },
   {
     "puja_id": 18,
@@ -141,7 +141,7 @@
     "type": "Ceremonial",
     "price": 6500,
     "description": "Performed to remove any Vastu Doshas and to invite peace and prosperity into the home or office.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/XyZZF_Yk0a6_1nzd3_Sagai_11zon-removebg-preview%20copy.png"
+    "image": "/pujaListImg/8.jpg"
   },
   {
     "puja_id": 19,
@@ -149,7 +149,7 @@
     "type": "Path [Recitation]",
     "price": 7000,
     "description": "A powerful ritual dedicated to Lord Shiva, performed to seek blessings for prosperity, health, and peace.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/5OyY3_AmcOc_Mahalaxmi%20Puja%20%281%29_11zon%20copy.png"
+    "image": "/pujaListImg/9.jpg"
   },
   {
     "puja_id": 20,
@@ -157,7 +157,7 @@
     "type": "Festivities",
     "price": 6000,
     "description": "A grand celebration of Goddess Durga, performed during Navratri to seek her blessings for protection.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/xaAre_f1qI6_GPPOy_Ganapati%20Havan%20copy_11zon%20%281%29.png"
+    "image": "/pujaListImg/10.png"
   },
   {
     "puja_id": 21,
@@ -165,7 +165,7 @@
     "type": "Death Rituals (Ancestral Rites)",
     "price": 5500,
     "description": "Performed to honor deceased ancestors, helping their souls attain peace and blessings for the family.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/wwyHU_tbIel_rc4Eh_IMG-20221020-WA0008%20copy_11zon_11zon.jpg"
+    "image": "/pujaListImg/1.png"
   },
   {
     "puja_id": 22,
@@ -173,7 +173,7 @@
     "type": "Death Rituals (Ancestral Rites)",
     "price": 6000,
     "description": "A ceremony to pay respect to ancestors, performed during the fortnight dedicated to the deceased in Hindu tradition.",
-    "image": "https://d35pcwl9m9kwu3.cloudfront.net/live/aw5Hc_Garud.jpg"
+    "image": "/pujaListImg/2.jpg"
   },
   {
     "puja_id": 23,
@@ -181,7 +181,7 @@
     "type": "Dosha Nivaran Havan & Jaap",
     "price": 8000,
     "description": "A special homam to increase longevity and ensure a healthy life, performed for both children and adults.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/XyZZF_Yk0a6_1nzd3_Sagai_11zon-removebg-preview%20copy.png"
+    "image": "/pujaListImg/3.jpg"
   },
   {
     "puja_id": 24,
@@ -189,7 +189,7 @@
     "type": "Dosha Nivaran Havan & Jaap",
     "price": 9000,
     "description": "A powerful ritual invoking Lord Sudarshana, performed to remove negative energies and protect.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/5OyY3_AmcOc_Mahalaxmi%20Puja%20%281%29_11zon%20copy.png"
+    "image": "/pujaListImg/4.jpg"
   },
   {
     "puja_id": 25,
@@ -197,7 +197,7 @@
     "type": "Business",
     "price": 7000,
     "description": "A ritual performed before the opening of a new business to seek blessings for success, prosperity, and longevity.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/xaAre_f1qI6_GPPOy_Ganapati%20Havan%20copy_11zon%20%281%29.png"
+    "image": "/pujaListImg/5.jpg"
   },
   {
     "puja_id": 26,
@@ -205,7 +205,7 @@
     "type": "Path [Recitation]",
     "price": 3000,
     "description": "A simple but powerful ritual to set an intention or resolve, performed before starting any new venture.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/wwyHU_tbIel_rc4Eh_IMG-20221020-WA0008%20copy_11zon_11zon.jpg"
+    "image": "/pujaListImg/6.jpg"
   },
   {
     "puja_id": 27,
@@ -213,7 +213,7 @@
     "type": "Path [Recitation]",
     "price": 5000,
     "description": "A powerful recitation of 700 verses dedicated to Goddess Durga, invoking her blessings for protection and success.",
-    "image": "https://d35pcwl9m9kwu3.cloudfront.net/live/aw5Hc_Garud.jpg"
+    "image": "/pujaListImg/7.jpg"
   },
   {
     "puja_id": 28,
@@ -221,7 +221,7 @@
     "type": "Path [Recitation]",
     "price": 8000,
     "description": "An elaborate recitation to invoke the blessings of Goddess Chandi, performed to overcome difficulties and enemies.",
-    "image": "https://d35pcwl9m9kwu3.cloudfront.net/live/PYEjQ_Kaala%20Sarpa%20Dosha%20Nivarana%20Homam.jpg"
+    "image": "/pujaListImg/8.jpg"
   },
   {
     "puja_id": 29,
@@ -229,7 +229,7 @@
     "type": "Path [Recitation]",
     "price": 12000,
     "description": "A continuous recitation of the entire Ramayana, performed to seek Lord Rama's blessings for peace.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/NfRiX_otjRy_IMG-20221020-WA0012%20copy_11zon.jpg"
+    "image": "/pujaListImg/9.jpg"
   },
   {
     "puja_id": 30,
@@ -237,7 +237,7 @@
     "type": "Path [Recitation]",
     "price": 3500,
     "description": "A recitation of Hanuman Chalisa, performed to seek strength, courage, and the blessings of Lord Hanuman.",
-    "image": "https://d2yiyjum1sxmcg.cloudfront.net/samskara/eRpRO_gabkz_mJ1xY_Durga%20Shapashati%20copy_11zon.png"
+    "image": "/pujaListImg/10.png"
   }
 ];
 
