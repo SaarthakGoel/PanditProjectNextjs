9db70fd3f90d6e18f40d106fb1c658d3139d111c
SaarthakGoel <saarthak145@gmail.com>
2024-10-20 14:54:30 +0530

    yes

--- src/app/api/fetchYTdata/route.js
+++ src/app/api/fetchYTdata/route.js
@@ -4,7 +4,7 @@ import { ytData } from "../../../../data";
 export async function fetchYTdata() {
   try{
     const res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=tophindibhajan&type=video&key=${process.env.YT_API_KEY}&maxResults=7`);
-    const data = ytData
+    //const data = ytData
     const data = await res.json();
     return data;
 
@@ -17,7 +17,7 @@ export async function fetchYTdata() {
 export async function fetchMoreYTdata(nextPageToken){
   try{
     const res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=bhajan&type=video&maxResults=3&key=${process.env.YT_API_KEY}&pageToken=${nextPageToken}`);
-    const data = ytData
+    //const data = ytData
     const data = await res.json();
     return data;
   }catch(err){
